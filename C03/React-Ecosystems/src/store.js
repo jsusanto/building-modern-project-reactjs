47e5476589273a04f9312d68d90baa7420f36123
Jeffry Susanto <jeffry.susanto@rmit.edu.au>
2022-07-01 22:51:20 +1000

    Chapter 3: Creating reducers

--- C03/React-Ecosystems/src/store.js
+++ C03/React-Ecosystems/src/store.js
@@ -1,6 +1,10 @@
 import { createStore, combineReducers } from 'redux';
+import { todos  } from './todos/reducers';
 
-const reducers = {};
+// Hook up todos with reducers
+const reducers = {
+    todos
+};
 
 // create root reducer
 const rootReducer = combineReducers (reducers);
---
+++ C03/React-Ecosystems/src/todos/reducers.js
@@ -0,0 +1,33 @@
+import { CREATE_TODO, REMOVE_TODO } from "./actions";
+
+// Add all reducers code
+/*
+ * Everytime any action is fired, reducers will get called.
+ * When this happens there are 2 arguments passed current state, action that was triggered which is object type and payload.
+ * What reducers will do is to take the current state and the action that was triggered,
+ * and decide what changes should occur in the state.
+ * Then it returns the updated state and redux will take that updated state.
+*/
+
+// Reducers function
+export const todos = (state = [], action) => {
+    const { type, payload } = action;
+
+    switch(type){
+        case CREATE_TODO:{
+            const { text } = payload;
+            const { newTodo } = {
+                text,
+                isCompleted: false,
+            };
+            return state.concat(newTodo);
+        }break;
+
+        case REMOVE_TODO:{
+            const { text } = payload;
+            return state.filter(todo => todo.text !== text);
+        }break;
+        default: 
+            return state;
+    }
+};
