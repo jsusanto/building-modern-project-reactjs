1eb85a4bae5d67c582ebc62535fa4488f64a5193
Jeffry Susanto <jeffry.susanto@rmit.edu.au>
2022-07-02 14:47:16 +1000

    Chapter 3: Persisting the Redux store

--- C03/React-Ecosystems/src/index.js
+++ C03/React-Ecosystems/src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { configureStore  } from './store.js';
 import App from './App.js';
+
+const store = configureStore();
+const persistor = persistStore(store);
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-    <Provider store={configureStore()}>
-        <App />
+    <Provider store={store}>
+        <PersistGate 
+            loading={<div>Loading...</div>}
+            persistor={persistor}>
+            <App />
+        </PersistGate>
     </Provider>,
     document.getElementById('root')
 );
--- C03/React-Ecosystems/src/store.js
+++ C03/React-Ecosystems/src/store.js
@@ -1,4 +1,7 @@
 import { createStore, combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/es/stateReconciler/autoMergeLevel2';
 import { todos  } from './todos/reducers';
 
 // Hook up todos with reducers
@@ -6,8 +9,17 @@ const reducers = {
     todos
 };
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    stateReconciler: autoMergeLevel2 // this is to tell Redux how to reconcile the initial and stored states of the application
+};
+
 // create root reducer
 const rootReducer = combineReducers (reducers);
 
+// persistConfig used to tell where to save the state
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 // return createStore
-export const configureStore = () => createStore(rootReducer);
+export const configureStore = () => createStore(persistedReducer);
